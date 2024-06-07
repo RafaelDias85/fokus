9b5d5417a250cabb8daed61e9e658552e17b86bc
Bowie <rafaeldiassilv@gmail.com>
2024-06-06 21:25:46 -0300

    feat: adicionando localStorage

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const audioPlay = new Audio("/sons/play.wav");
 const audioPausa = new Audio("/sons/pause.mp3");
 const audioTempoFinalizado = new Audio("/sons/beep.mp3");
 
-let tempoDecorridoEmSegundos = 1500;
+let tempoDecorridoEmSegundos = 10;
 let intevaloId = null;
 
 musica.loop = true;
@@ -81,7 +81,6 @@ function alterarContexto(contexto) {
 const contagemRegressiva = () => {
   if (tempoDecorridoEmSegundos <= 0) {
     audioTempoFinalizado.play();
-    alert("Tempo Finalizado");
     zerar();
     return;
   }
